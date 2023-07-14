177fc299b545192ada7db65ed5c3fe6c59021420
nad <jnadeu@gmail.com>
2023-07-14 14:11:21 +0200

    added test.py file (WIP)

---
+++ tests.py
@@ -0,0 +1,15 @@
+
+
+class Tests:
+    """ Class where define all hardware tests.
+        List of test:
+            1. SMART (more info..)
+    """
+
+    def run():
+        """ Main function to start all hardware tests."""
+        tests_data = []
+        return tests_data
+
+    def smart_test():
+        """ Define HDD/SSD smart test"""
